f38e61b232f7ca1d72592f64156d8822cee80f8b
Caleb Buening <caleb.j.buening@gmail.com>
2021-10-28 08:41:53 -0400

    Gamemode selection
    and also we made the car selection png

--- controller.py
+++ controller.py
@@ -1,13 +1,22 @@
 import turtle as trtl
 
 class Controller:
-    def clickHandler(self, x, y):
+    def titleScreenClickHandler(self, x, y):
         if x > -12 and x < 310:
             if y < 105 and y > -4:
                 self.buttonPressed = True
             if y > -164 and y < -49:
                 self.buttonPressed = True
                 self.cont = False
+
+    def gamemodeClickHandler(self, x, y):
+        if y > -164 and y < 134:
+            if x > -330 and x < -30:
+                self.buttonPressed = True
+                self.gamemode = 1
+            if x > 3 and x < 312:
+                self.buttonPressed = True
+                self.gamemode = 2
 
     def __init__(self):
         self.wn = trtl.Screen()
@@ -16,38 +25,62 @@ class Controller:
 
         self.buttonPressed = False
         self.cont = True
+        self.gamemode = 0
         self.windowxy = (750, 500)
 
         self.cv = trtl.getcanvas()
     
     def chooseGameMode(self):
         trtl.clearscreen()
+
+        self.wn.bgpic("backgrounds/gamemode_selection.png")
+        self.wn.onclick(self.gamemodeClickHandler)
+        self.wn.listen()
+
+        while not self.buttonPressed:
+            trtl.update()
+            trtl.delay(10)
+
+        # Reset click handler
+        self.wn.onclick(None)
+        self.wn.listen()
+        self.buttonPressed = False
+        return self.gamemode
+
         # Go fullscreen and store dimensions
-        self.wn.setup(width=1.0, height = 1.0)
-        self.windowxy = self.wn.screensize()
-        trtl.mainloop()
+        # self.wn.setup(width=1.0, height = 1.0)
+        # self.windowxy = self.wn.screensize()
 
     def titleScreen(self):
         # Listen for the click on either button
-        self.wn.onclick(self.clickHandler)
+        self.wn.onclick(self.titleScreenClickHandler)
         self.wn.listen()
+
+        self.wn.bgpic("backgrounds/title_screen.png")
 
         # Choose each bg based off of mouse location
         while not self.buttonPressed:
-            x, y = self.cv.winfo_pointerxy()
-            if x > 364+595 and x < 685+595:
-                if y > 144+325 and y < 255+325:
-                    self.wn.bgpic("backgrounds/title_screen_select_top.png")
-                elif y > 301+325 and y < 413+325:
-                    self.wn.bgpic("backgrounds/title_screen_select_bottom.png")
-                else:
-                    self.wn.bgpic("backgrounds/title_screen.png")
-            else:
-                self.wn.bgpic("backgrounds/title_screen.png")
+            # x, y = self.cv.winfo_pointerxy()
+            # if x > 364+595 and x < 685+595:
+            #     if y > 144+325 and y < 255+325:
+            #         self.wn.bgpic("backgrounds/title_screen_select_top.png")
+            #     elif y > 301+325 and y < 413+325:
+            #         self.wn.bgpic("backgrounds/title_screen_select_bottom.png")
+            #     else:
+            #         self.wn.bgpic("backgrounds/title_screen.png")
+            # else:
+            #     self.wn.bgpic("backgrounds/title_screen.png")
             trtl.update()
             trtl.delay(10)
+        self.buttonPressed = False # Reset the value
 
         # Clear the listener
         self.wn.onclick(None)
         self.wn.listen()
         return self.cont
+    def chooseCars(self, gm):
+        trtl.clearscreen()
+
+
+
+        trtl.mainloop()
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ ctrl = controller.Controller()
 
 # Load the titlescreen, which will continue until the user presses play
 if ctrl.titleScreen(): # If this returns false the player doesn't want to continue
-    while True:
-        gameMode = ctrl.chooseGameMode()
-        # ctrl.chooseCars(gameMode)
+    # while True:
+    gameMode = ctrl.chooseGameMode()
+    ctrl.chooseCars(gameMode)
 
-        playAgain = True
-        while playAgain:
-            map = ctrl.chooseMap()
-            # playAgain = ctrl.runGame(gameMode, map)
+    # playAgain = True
+    # while playAgain:
+    #     map = ctrl.chooseMap()
+    #     playAgain = ctrl.runGame(gameMode, map)
